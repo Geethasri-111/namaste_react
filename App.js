abda52007d3ec9719dbeadd6d81fc91658a3da19
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-03-28 19:52:15 +0530

    includeparcel and react modules

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+
 const heading = React.createElement('div',{id:"parent"},[
-    React.createElement('div',{id:"child1"},[
-        React.createElement('h1',{id:"heading",class:"glow"}, 'h1 tag'),
-        React.createElement('h2',{id:"heading2",class:"glow"}, 'h2 tag'),
+    React.createElement('div',{id:"childd"},[
+        React.createElement('h1',{id:"heading",className:"glow"}, 'h1 tag'),
+        React.createElement('h2',{id:"heading2",className:"glow"}, 'h2 tag'),
     ]),
     React.createElement('div',{id:"child2"},[
-    React.createElement('h1',{id:"heading",class:"glow"}, 'h1 tag'),
-    React.createElement('h2',{id:"heading2",class:"glow"}, 'h2 tag'),
+    React.createElement('h1',{id:"heading",className:"glow"}, 'h1 tag'),
+    React.createElement('h2',{id:"heading2",className:"glow"}, 'h change tag'),
 ])
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 this  is a sample react project
+
+#parcel
+-DEvbuild
+-gives a local server
+-refresh automatically -HMR
+-file watching Algorithm -C++
+-caches
+-minification
+-compression
+-image optimization
+-Bundles
+-consistent hashing
+-code splitting
+-differential bundling 
+-diagnosis
+-Error Handling
+-Https.
+-Treeshaking-removes unused code
+-diffreent dev nd prod builds
+
