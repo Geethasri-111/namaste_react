a54df356a3f5b01a40e84f8ae48d4b87fdab76f5
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-03-29 16:02:19 +0530

    jsx intro

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 
-const heading = React.createElement('div',{id:"parent"},[
-    React.createElement('div',{id:"childd"},[
-        React.createElement('h1',{id:"heading",className:"glow"}, 'h1 tag'),
-        React.createElement('h2',{id:"heading2",className:"glow"}, 'h2 tag'),
-    ]),
-    React.createElement('div',{id:"child2"},[
-    React.createElement('h1',{id:"heading",className:"glow"}, 'h1 tag'),
-    React.createElement('h2',{id:"heading2",className:"glow"}, 'h change tag'),
-])
-]);
+const jsxhead = <h1>jsx line</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+root.render(jsxhead);
