cf7185231f857cf9eeee3e52ba546d7fb14fcec2
Geethasri-111 <61574935+Geethasri-111@users.noreply.github.com>
2025-05-09 14:54:55 +0530

    adding about user through class setState

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -4,14 +4,22 @@ class User extends React.Component{
         super(props);
         this.state = {
             count :0,
+            userData : {
+                name :'Dummy',location:'US',avatar_url:'dumy'
+            }
         }
         console.log(this.props.name+'constructor')
     }
-    componentDidMount(){
-        console.log(this.props.name+'component Mount')
+    async componentDidMount(){
+        console.log(this.props.name+'component Mount');
+        const data = await fetch('https://api.github.com/users/akshay');
+        const json = await data.json();
+        this.setState({
+            userData :json
+        })
     }
     render(){
-        const {name,location} = this.props;
+        const {name,location,avatar_url} = this.state.userData;
         console.log(this.props.name+'render')
     return (
         <div>
@@ -19,6 +27,7 @@ class User extends React.Component{
             <button onClick={()=>{
                 this.setState({count :this.state.count+1})
             }}>Increase</button>
+            <img src={avatar_url} alt="avatar"/>
             <h1>name : {name}</h1>
             <p>{location}</p>
         </div>
